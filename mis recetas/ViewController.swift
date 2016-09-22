69a017a58d86788d976065997051f826e3e6cb69
Spot Matic SL <spotmaticsl@spots-macbook.local>
2016-09-22 17:23:12 +0200

    Initial Commit

---
+++ mis recetas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mis recetas
+//
+//  Created by Spot Matic SL on 22/09/16.
+//  Copyright © 2016 Spot Matic SL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
